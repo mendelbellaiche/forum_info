2a7c0e8f438f0a3fff32dcd281f74acef006db85
mendelbellaiche <58737343+mendelbellaiche@users.noreply.github.com>
2020-06-16 02:31:03 +0300

    Change Label
    Change Label for dataset

--- static/js/chart.js
+++ static/js/chart.js
@@ -9,7 +9,7 @@ $(function() {
                 data: {
                   labels: ["Number of Tickets", "Number of Comments", "Total"],
                   datasets: [{
-                    label: "Population (millions)",
+                    label: "Number of Item",
                     backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
                     data: [data.number_tickets, data.number_comments, data.number_tickets + data.number_comments]
                   }]
