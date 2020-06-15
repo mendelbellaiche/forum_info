779a59dadf4bd57fc6db277aaf82f1dba9af7bcc
mendelbellaiche <58737343+mendelbellaiche@users.noreply.github.com>
2020-06-16 02:29:29 +0300

    Split up the JS code
    Create a JS File and call it from file source

---
+++ static/js/chart.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    $.ajax({
+        url: '../data/',
+        success: function (data) {
+
+            new Chart(document.getElementById("pie-chart"), {
+                type: 'pie',
+                data: {
+                  labels: ["Number of Tickets", "Number of Comments", "Total"],
+                  datasets: [{
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+                    data: [data.number_tickets, data.number_comments, data.number_tickets + data.number_comments]
+                  }]
+                },
+                options: {
+                  title: {
+                    display: true,
+                    text: 'Number of Tickets and Comments'
+                  }
+                }
+            });
+
+        }
+    });
+
+});
